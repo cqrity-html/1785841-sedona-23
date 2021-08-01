cbf271a68fa041ad27c4742e27ea7fa2527970b8
Алексей <cqrity@list.ru>
2021-08-01 18:33:44 +0300

    Сетки всех страниц

--- source/js/script.js
+++ source/js/script.js
@@ -1,6 +1,19 @@
 let mainNav = document.querySelector('.main-nav__list');
 let mainNavToggle = document.querySelector('.main-nav__toggle');
-let mainNavClose = document.querySelector('.main-nav__close');
+let mainNavClose = document.querySelector('.main-nav__close-button');
+
+let modal = document.querySelector('.modal');
+let modalError = document.querySelector('.modal--error');
+let modalSuccess = document.querySelector('.modal--success');
+let modalButtonClose = document.querySelector('.modal__button--close');
+let modalButtonOk = document.querySelector('.modal__button--ok');
+
+let feedbackButton = document.querySelector('.feedback-form__button');
+let feedbackForm = document.querySelector(".feedback-form");
+let feedbackName = document.querySelector(".feedback-form__label--first-name");
+let feedbackFamilyName = document.querySelector(".feedback-form__label--family-name");
+let feedbackPhone = document.querySelector(".feedback-form__field--phone");
+let feedbackEmail = document.querySelector(".feedback-form__field--email");
 
 //MAIN-NAV
 
@@ -9,12 +22,10 @@ if (mainNav && mainNavToggle && mainNavClose) {
   mainNavToggle.addEventListener('click', function (event) {
     event.preventDefault();
     mainNav.classList.remove('visually-hidden');
-    mainNav.classList.add('modal-appear');
   });
 
   mainNavClose.addEventListener('click', function () {
     mainNav.classList.add('visually-hidden');
-    mainNav.classList.remove('modal-appear');
   });
 
   window.addEventListener("keydown", function (evt) {
@@ -22,8 +33,47 @@ if (mainNav && mainNavToggle && mainNavClose) {
       if (!mainNav.classList.contains("visually-hidden")) {
         evt.preventDefault();
         mainNav.classList.add("visually-hidden");
-        mainNav.classList.remove('modal-appear');
       }
     }
   });
+}
+
+//MAIN-NAV
+
+if (modal && modalError && modalSuccess && modalButtonClose && modalButtonOk && feedbackButton && feedbackForm && feedbackName && feedbackPhone && feedbackEmail && feedbackFamilyName) {
+
+  feedbackForm.addEventListener("submit", function (evt) {
+    if (!feedbackName.value || !feedbackEmail.value || !feedbackPhone.value) {
+      evt.preventDefault();
+      modalError.classList.remove("visually-hidden");
+      modalFeedback.offsetWidth = feedbackForm.offsetWidth;
+    }
+  });
+
+  feedbackForm.addEventListener("submit", function (evt) {
+    if (feedbackName.value || feedbackEmail.value || feedbackPhone.value) {
+      evt.preventDefault();
+      modalSuccess.classList.remove("visually-hidden");
+      modalError.classList.add("visually-hidden");
+      modalFeedback.offsetWidth = feedbackForm.offsetWidth;
+    }
+  });
+
+  window.addEventListener("keydown", function (evt) {
+    if (evt.key === 'Esc' || evt.key === 'Escape') {
+      if (!modal.classList.contains("visually-hidden")) {
+        evt.preventDefault();
+        modal.classList.add("visually-hidden");
+      }
+    }
+  });
+
+  modalButtonClose.addEventListener('click', function () {
+    modalSuccess.classList.add('visually-hidden');
+  });
+
+  modalButtonOk.addEventListener('click', function () {
+    modalError.classList.add('visually-hidden');
+  });
+
 }
